742ef4d494984f6e45ec1fb03122fb63a86cbd9e
Leannnmv <leandroch2002@gmail.com>
2024-12-10 10:13:46 -0300

    Added exception handling to main.py and a new confirmation window to interface.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,29 @@
+"""
+Main Script for Directory Organizer
+
+Este programa organiza los archivos en un directorio seleccionado por el usuario.
+"""
+import sys
+import logging
+from organizer import organize_directory
 from interface import get_folder_path
-import organizer as org
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
-    folder_path = get_folder_path()
-    
-    if folder_path:
-        org.create_folders(folder_path)
-        org.organize_directory(folder_path)
+    try:
+        folder_path = get_folder_path()
+        if folder_path:
+            logging.info(f"Carpeta seleccionada: {folder_path}")
+            organize_directory(folder_path)
+            logging.info("Organización completada con éxito.")
+        else:
+            print("No se seleccionó ninguna carpeta.")
+    except Exception as e:
+        logging.error(f"Error inesperado: {e}")
+    finally:
+        logging.info("Finalizando el programa.")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
